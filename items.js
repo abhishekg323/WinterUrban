d1e9215d4695de9c738b3b709b2ffa1c68ab7b1f
abhishek gupta <abhishekg323@gmail.com>
2020-12-27 09:43:03 +0530

    Animations Added + popup for adding Item to cart

--- items.js
+++ items.js
@@ -4,4 +4,5 @@ addItemsToCart = (id) => {
     itemsInCart.push(id);
     console.log(id);
     document.getElementById("itemsInCart").textContent = itemsInCart.length;
+    alert("Item added to Cart");
 }
